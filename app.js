359a53a409d2f27e53ee41e572f8ee37b63adf0c
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-07-15 22:38:05 -0400

    inquirer

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
-const generatePage = require('./src/page-template.js');
-const fs = require('fs');
+const inquirer = require('inquirer')
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
 
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
-
-
-
-//========================================================================================================================================
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-
-// ========================================================================================================================================
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if(err) throw new Error(err);
-
-    console.log('Portefolio complete! Check out index.html to see the output!');   
-});
- 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name'
+    }
+  ])
+  .then(answers => console.log(answers));
